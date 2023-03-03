6f29497b46d3f13da73bbefd8541586794bff1d4
iampooran <pooransen50@gmail.com>
2023-03-03 13:17:43 +0530

    Input Compelete

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-// import AutoCompleteExample from "./components/AutoCompleteExample";
-// import ButtonExample from "./components/ButtonExample";
-// import ButtonGroupExample from "./components/ButtonGroupExample";
-// import CheckBoxExample from "./components/CheckBoxExample";
-// import FloatingActionButtonExample from "./components/FloatingActionButtonExample";
-// import RadioGroupExample from "./components/RadioGroupExample";
-// import RatingExample from "./components/RatingExample";
-// import SelectExample from "./components/SelectExample";
-// import SliderExample from "./components/SliderExample";
-// import SwitchExample from "./components/SwitchExample";
-// import TextFieldExample from "./components/TextFieldExample";
-// import TransferListExample from "./components/TransferListExample";
+import Inputs from "./Inputs";
 
-import ToggleButtonExample from "./components/ToggleButtonExample";
 
 
 function App() {
   return (
     <div>
-      {/* <ButtonExample/> */}
-      {/* <AutoCompleteExample/> */}
-      {/* <ButtonGroupExample/> */}
-      {/* <CheckBoxExample /> */}
-      {/* <FloatingActionButtonExample/> */}
-      {/* <RadioGroupExample/> */}
-      {/* <RatingExample /> */}
-      {/* <SelectExample/> */}
-      {/* <SliderExample/> */}
-      {/* <SwitchExample/> */}
-      {/* <TextFieldExample/> */}
-      {/* <TransferListExample/> */}
-      <ToggleButtonExample/>
+      <Inputs/>
     </div>
   );
 }
---
+++ src/Inputs.jsx
@@ -0,0 +1,35 @@
+import ButtonExample from "./components/Inputs/ButtonExample";
+import AutoCompleteExample from "./components/Inputs/AutoCompleteExample";
+import ButtonGroupExample from "./components/Inputs/ButtonGroupExample";
+import CheckBoxExample from "./components/Inputs/CheckBoxExample";
+import FloatingActionButtonExample from "./components/Inputs/FloatingActionButtonExample";
+import RadioGroupExample from "./components/Inputs/RadioGroupExample";
+import RatingExample from "./components/Inputs/RatingExample";
+import SelectExample from "./components/Inputs/SelectExample";
+import SliderExample from "./components/Inputs/SliderExample";
+import SwitchExample from "./components/Inputs/SwitchExample";
+import TextFieldExample from "./components/Inputs/TextFieldExample";
+import ToggleButtonExample from "./components/Inputs/ToggleButtonExample";
+import TransferListExample from "./components/Inputs/TransferListExample";
+
+const Inputs = () => {
+  return (
+    <div>
+      <ButtonExample />
+      <AutoCompleteExample />
+      <ButtonGroupExample />
+      <CheckBoxExample />
+      <FloatingActionButtonExample />
+      <RadioGroupExample />
+      <RatingExample />
+      <SelectExample />
+      <SliderExample />
+      <SwitchExample />
+      <TextFieldExample />
+      <TransferListExample />
+      <ToggleButtonExample />
+    </div>
+  );
+};
+
+export default Inputs;
