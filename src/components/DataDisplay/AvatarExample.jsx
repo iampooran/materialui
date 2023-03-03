2499897d5718bbae8729f121eb18d306dc28fa50
iampooran <pooransen50@gmail.com>
2023-03-03 13:27:18 +0530

    Avatar Added

--- src/components/DataDisplay/AvatarExample.jsx
+++ src/components/DataDisplay/AvatarExample.jsx
@@ -1,7 +1,52 @@
+import { Avatar, Stack } from "@mui/material";
 import React from "react";
+import { deepOrange, deepPurple } from "@mui/material/colors";
+import FolderIcon from "@mui/icons-material/Folder";
+import PageviewIcon from "@mui/icons-material/Pageview";
+import AssignmentIcon from "@mui/icons-material/Assignment";
+import { green, pink } from "@mui/material/colors";
 
 const AvatarExample = () => {
-  return <div>AvatarExample</div>;
+  return (
+    <div>
+      <div>
+        <p>Image Avatar</p>
+        <Stack direction="row" spacing={2}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://images.unsplash.com/photo-1511367461989-f85a21fda167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80  "
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.unsplash.com/photo-1529665253569-6d01c0eaf7b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1085&q=80"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80"
+          />
+        </Stack>
+      </div>
+      <p>Text Avatar</p>
+      <Stack direction="row" spacing={2}>
+        <Avatar>H</Avatar>
+        <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+        <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+      </Stack>
+
+      <p>Icon Avatar</p>
+      <Stack direction="row" spacing={2}>
+        <Avatar>
+          <FolderIcon />
+        </Avatar>
+        <Avatar sx={{ bgcolor: pink[500] }}>
+          <PageviewIcon />
+        </Avatar>
+        <Avatar sx={{ bgcolor: green[500] }}>
+          <AssignmentIcon />
+        </Avatar>
+      </Stack>
+    </div>
+  );
 };
 
 export default AvatarExample;
